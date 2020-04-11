26e489c8c911bb19314e0612940032f58f12d5f0
melnyczuk <h.melnyczuk@gmail.com>
2020-04-11 10:30:46 +0100

    ✨ add GET /link route

--- build/index.js
+++ build/index.js
@@ -7,39 +7,58 @@ var __importDefault =
 Object.defineProperty(exports, '__esModule', { value: true });
 const axios_1 = __importDefault(require('axios'));
 const express_1 = __importDefault(require('express'));
+const cors_1 = __importDefault(require('cors'));
 const ytsr_1 = __importDefault(require('ytsr'));
 const ytdl_core_1 = __importDefault(require('ytdl-core'));
 const app = express_1.default();
-const PORT = parseInt(process.env.PORT || '3000', 10);
-app.listen(PORT, '0.0.0.0', () => console.log('server running'));
-const getVidLink = async num => {
+const origin = process.env.DEV
+  ? 'http://localhost:3000'
+  : 'https://sites.melnycz.uk';
+const PORT = parseInt(process.env.PORT || '3001', 10);
+app.use(cors_1.default({ origin, 'Access-Control-Allow-Origin': origin }));
+app.listen(PORT, '0.0.0.0', () => console.log(`server running port: ${PORT}`));
+const rN = seed => Math.floor(Math.random() * seed);
+const newUrl = async () => {
+  const num = rN(10000);
   const { items } = await ytsr_1.default(`IMG${num}`);
-  const randIndex = Math.floor(Math.random() * items.length);
-  return items[randIndex].link;
+  const { link } = items[rN(items.length)];
+  const {
+    formats: [{ url }],
+  } = await ytdl_core_1.default.getInfo(link);
+  return url;
 };
-const getVidInfo = async link => {
-  const { formats } = await ytdl_core_1.default.getInfo(link);
-  return formats[0].url;
+const getLink = async res => {
+  try {
+    const url = await newUrl();
+    console.log('link:', url);
+    res.send(url);
+  } catch (e) {
+    console.log('error:', e);
+    getLink(res);
+  }
 };
-const getVid = async res => {
-  const numb = Math.floor(Math.random() * 10000);
-  getVidLink(numb)
-    .then(getVidInfo)
-    .then(url =>
-      axios_1
-        .default({
-          method: 'get',
-          responseType: 'stream',
-          url,
-        })
-        .then(resp => {
-          res.setHeader('content-length', resp.headers['content-length']);
-          res.setHeader('content-type', resp.data.headers['content-type']);
-          resp.data.pipe(res);
-        }),
-    )
-    .catch(() => getVid(res));
+const getPipe = async res => {
+  try {
+    const url = await newUrl();
+    const resp = await axios_1.default({
+      method: 'get',
+      responseType: 'stream',
+      url,
+    });
+    res.setHeader('content-length', resp.headers['content-length']);
+    res.setHeader('content-type', resp.data.headers['content-type']);
+    console.log('pipe:', url);
+    resp.data.pipe(res);
+  } catch (e) {
+    console.log('error:', e);
+    getPipe(res);
+  }
 };
-app.get('/', async function(req, res) {
-  getVid(res);
+app.get('/', async function(_, res) {
+  await getPipe(res);
+  console.log('sent');
+});
+app.get('/link', async function(_, res) {
+  await getLink(res);
+  console.log('sent');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,62 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import express, { Response } from 'express';
+import cors from 'cors';
 import ytsr from 'ytsr';
 import ytdl from 'ytdl-core';
 
 const app = express();
 
-const PORT: number = parseInt(process.env.PORT || '3000', 10);
+const origin = process.env.DEV
+  ? 'http://localhost:3000'
+  : 'https://sites.melnycz.uk';
 
-app.listen(PORT, '0.0.0.0', () => console.log('server running'));
+const PORT: number = parseInt(process.env.PORT || '3001', 10);
+app.use(cors({ origin, 'Access-Control-Allow-Origin': origin }));
+app.listen(PORT, '0.0.0.0', () => console.log(`server running port: ${PORT}`));
 
-const getVidLink = async (num: number): Promise<string> => {
+const rN = (seed: number): number => Math.floor(Math.random() * seed);
+
+const newUrl = async (): Promise<string> => {
+  const num = rN(10000);
   const { items } = await ytsr(`IMG${num}`);
-  const randIndex = Math.floor(Math.random() * items.length);
-  return items[randIndex].link;
+  const { link } = items[rN(items.length)];
+  const {
+    formats: [{ url }],
+  } = await ytdl.getInfo(link);
+  return url;
 };
 
-const getVidInfo = async (link: string): Promise<string> => {
-  const { formats } = await ytdl.getInfo(link);
-  return formats[0].url;
+const getLink = async (res: Response<string>): Promise<void> => {
+  try {
+    const url = await newUrl();
+    console.log('link:', url);
+    res.send(url);
+  } catch (e) {
+    console.log('error:', e);
+    getLink(res);
+  }
 };
 
-const getVid = async (res: Response<any>): Promise<void> => {
-  const numb = Math.floor(Math.random() * 10000);
-  getVidLink(numb)
-    .then(getVidInfo)
-    .then(url =>
-      axios({
-        method: 'get',
-        responseType: 'stream',
-        url,
-      }).then(resp => {
-        res.setHeader('content-length', resp.headers['content-length']);
-        res.setHeader('content-type', resp.data.headers['content-type']);
-        resp.data.pipe(res);
-      }),
-    )
-    .catch(() => getVid(res));
+const getPipe = async (res: Response<AxiosResponse['data']>): Promise<void> => {
+  try {
+    const url = await newUrl();
+    const resp = await axios({ method: 'get', responseType: 'stream', url });
+    res.setHeader('content-length', resp.headers['content-length']);
+    res.setHeader('content-type', resp.data.headers['content-type']);
+    console.log('pipe:', url);
+    resp.data.pipe(res);
+  } catch (e) {
+    console.log('error:', e);
+    getPipe(res);
+  }
 };
 
-app.get('/', async function(req, res) {
-  getVid(res);
+app.get('/', async function(_, res) {
+  await getPipe(res);
+  console.log('sent');
+});
+
+app.get('/link', async function(_, res) {
+  await getLink(res);
+  console.log('sent');
 });
