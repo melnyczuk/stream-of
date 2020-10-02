345c0f1c1c8a7a2107681c7f0915b5989fca4ee9
how melnyczuk <how@melnycz.uk>
2020-10-02 13:38:22 +0100

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,26 @@
 import axios, { AxiosResponse } from 'axios';
-import express, { Response } from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import ytsr from 'ytsr';
 import ytdl from 'ytdl-core';
 
 const app = express();
 
-const origin = process.env.DEV
-  ? 'http://localhost:3000'
-  : 'https://sites.melnycz.uk';
+const { ORIGIN, PORT } = process.env;
 
-const PORT: number = parseInt(process.env.PORT || '3001', 10);
-app.use(cors({ origin, 'Access-Control-Allow-Origin': origin }));
-app.listen(PORT, '0.0.0.0', () => console.log(`server running port: ${PORT}`));
+app.use(cors({ origin: ORIGIN, 'Access-Control-Allow-Origin': ORIGIN }));
+
+app.listen(parseInt(PORT || '8080', 10), '0.0.0.0', () =>
+  console.log(`server running port: ${PORT}`),
+);
 
 const rN = (seed: number): number => Math.floor(Math.random() * seed);
 
 const newUrl = async (): Promise<string> => {
-  const num = rN(10000);
-  const { items } = await ytsr(`IMG${num}`);
+  const { items } = await ytsr(`IMG${rN(10000)}`);
   const { link } = items[rN(items.length)];
-  const {
-    formats: [{ url }],
-  } = await ytdl.getInfo(link);
-  return url;
+  const { formats } = await ytdl.getInfo(link);
+  return formats[0].url;
 };
 
 const getLink = async (res: Response<string>): Promise<void> => {
@@ -40,23 +37,30 @@ const getLink = async (res: Response<string>): Promise<void> => {
 const getPipe = async (res: Response<AxiosResponse['data']>): Promise<void> => {
   try {
     const url = await newUrl();
-    const resp = await axios({ method: 'get', responseType: 'stream', url });
-    res.setHeader('content-length', resp.headers['content-length']);
-    res.setHeader('content-type', resp.data.headers['content-type']);
+    const response = await axios({
+      method: 'get',
+      responseType: 'stream',
+      url,
+    });
+    res.setHeader('content-length', response.headers['content-length']);
+    res.setHeader('content-type', response.data.headers['content-type']);
     console.log('pipe:', url);
-    resp.data.pipe(res);
+    response.data.pipe(res);
   } catch (e) {
     console.log('error:', e);
     getPipe(res);
   }
 };
 
-app.get('/', async function(_, res) {
+app.get('/', async function(_: Request, res: Response<AxiosResponse['data']>) {
   await getPipe(res);
   console.log('sent');
 });
 
-app.get('/link', async function(_, res) {
+app.get('/link', async function(
+  _: Request,
+  res: Response<AxiosResponse['data']>,
+) {
   await getLink(res);
   console.log('sent');
 });
